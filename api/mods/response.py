4233b94a1822fae8b9bd0f8db5bd3d6ed46ff36e
yxm <yxm.dev@gmx.ie>
2026-01-14 12:40:53 -0300

    fix: minor fixing

--- api/mods/response.py
+++ api/mods/response.py
@@ -1,19 +1,19 @@
-from typed import model, Enum, Str, Int, List, Dict, Union, typed
+from typed import model, Enum, Str, typed, Maybe
 from utils.types import Json, Nat
 
 
 @model
 class Response:
     status: Enum(Str, "success", "failure")
-    code: Int
-    data: Union(Str, List, Dict)
-
+    code: Nat
+    data: Maybe(Json)=None
+    message: Maybe(Str)=None
 
 class response:
     @typed
-    def success(data: Json={}, code: Nat=200) -> Response:
-        return Response(status="success", code=code, data=data)
+    def success(code: Nat=200, message: Maybe(Str)=None, data: Maybe(Json)=None) -> Response:
+        return Response(status="success", message=message, code=code, data=data)
 
     @typed
-    def failure(data: Json = {}, code: Nat = 400) -> Response:
+    def failure(code: Nat=400, message: Maybe(Str)=None, data: Maybe(Json)=None) -> Response:
         return Response(status="failure", code=code, data=data)
